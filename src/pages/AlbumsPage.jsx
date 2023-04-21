d385a61522d856613d1aaa861c121b08b3a1620a
GTX155 <kirchoabv@gmail.com>
2023-04-21 22:52:05 +0300

    now loads the the description correctly

--- src/pages/AlbumsPage.jsx
+++ src/pages/AlbumsPage.jsx
@@ -2,34 +2,30 @@ import { Header } from "/src/components/Header";
 import { MyAlbums } from "/src/components/MyAlbums";
 import RockData from "/src/Rock.json"
 import MetalData from "/src/Metal.json"
-import RockDesc from "/src/RockDesc.json"
 import './StyleAlbums.css'
-
 
 export function AlbumsPage (){
     return (
         <>
             <Header />
-            <MyMusicPages />
+            <GenresTextDataLoader />
         </>
     )
 }
 
-function MyMusicPages() {
+function GenresTextDataLoader() {
   const ROCKDATA = RockData;
   const METALDATA = MetalData;
-  const ROCKDESC = RockDesc;
 
   // Merge the two arrays of objects into one
-  const combinedMusicData = [...ROCKDATA, ...METALDATA, ...ROCKDESC];
+  const combinedMusicData = [...ROCKDATA, ...METALDATA,];
 
   return (
     <>
       {combinedMusicData.map((music, index) => {
         // Check if music.subRock is defined, otherwise check music.subMetal, and fallback to empty string
-        const name = music.subRock || music.subMetal || '⚠️Title';
-        const desc = music.descRock || '⚠️Bacon ipsum dolor amet landjaeger buffalo dolore turkey. Nostrud exercitation sunt, aliquip fatback leberkas consequat.';
-        console.log(music.descRock);
+        const name = music.subRock || music.subMetal;
+        const desc = music.descRock || music.descMetal;
 
         return (
           <MyAlbums key={index} name={name} desc={desc} id={name.replace(/\s/g, '')}/>
