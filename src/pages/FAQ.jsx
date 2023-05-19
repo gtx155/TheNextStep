04312480e288dec56c934684c60ba024802983b1
GTX155 <kirchoabv@gmail.com>
2023-05-19 22:46:06 +0300

    added more albums to the rock and metal json, more minor additions

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -5,20 +5,37 @@ export function FAQ (){
     return (
     <>
     <Header />
-    <div className="genreOne-content-container border-gradient border-gradient-purple" id='1'>
+    <h1 style={{color: 'white', textAlign: 'center'}}>FAQ</h1>
+    <div className="genreOne-content-container border-gradient border-gradient-purple">
         <div className="genreTilte border-gradient border-gradient-purple only-bottom">
-            <h1>Why are the albums oredered in this way?</h1>
+            <h1>Why are the albums ordered in this way?</h1>
         </div>
         <div className="genreDescBox border-gradient border-gradient-purple only-bottom">
             <p>Albums are generally ordered from the most to least accessible.</p>
         </div>
     </div>
-    <div className="genreOne-content-container border-gradient border-gradient-purple" id='1'>
+    <div className="genreOne-content-container border-gradient border-gradient-purple">
         <div className="genreTilte border-gradient border-gradient-purple only-bottom">
-            <h1>Why is X album in X genre</h1>
+            <h1>Bacon ipsum dolor amet pork andouille dolor picanha proident ball tip spare ribs?</h1>
         </div>
         <div className="genreDescBox border-gradient border-gradient-purple only-bottom">
-            <p>Albums are sorted not by </p>
+            <p>Beef filet mignon pork belly commodo lorem ea aute eiusmod do.</p>
+        </div>
+    </div>
+    <div className="genreOne-content-container border-gradient border-gradient-purple">
+        <div className="genreTilte border-gradient border-gradient-purple only-bottom">
+            <h1>Bacon ipsum dolor amet pork andouille dolor picanha proident ball tip spare ribs?</h1>
+        </div>
+        <div className="genreDescBox border-gradient border-gradient-purple only-bottom">
+            <p>Beef filet mignon pork belly commodo lorem ea aute eiusmod do.</p>
+        </div>
+    </div>
+    <div className="genreOne-content-container border-gradient border-gradient-purple">
+        <div className="genreTilte border-gradient border-gradient-purple only-bottom">
+            <h1>Bacon ipsum dolor amet pork andouille dolor picanha proident ball tip spare ribs?</h1>
+        </div>
+        <div className="genreDescBox border-gradient border-gradient-purple only-bottom">
+            <p>Beef filet mignon pork belly commodo lorem ea aute eiusmod do.</p>
         </div>
     </div>
         <Footer page={"home"}/>
