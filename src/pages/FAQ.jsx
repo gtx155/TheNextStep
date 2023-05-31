00b58b099b41e6f42d64766e5059b66944abb830
GTX155 <kirchoabv@gmail.com>
2023-05-31 22:48:50 +0300

    new albums, removed some place holders

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -23,31 +23,7 @@ export function FAQ (){
             <p>Beef filet mignon pork belly commodo lorem ea aute eiusmod do.</p>
         </div>
     </div>
-    <div className="genreOne-content-container border-gradient border-gradient-purple">
-        <div className="genreTilte border-gradient border-gradient-purple only-bottom">
-            <h1>Bacon ipsum dolor amet pork andouille dolor picanha proident ball tip spare ribs?</h1>
-        </div>
-        <div className="genreDescBox border-gradient border-gradient-purple only-bottom">
-            <p>Beef filet mignon pork belly commodo lorem ea aute eiusmod do.</p>
-        </div>
-    </div>
-    <div className="genreOne-content-container border-gradient border-gradient-purple">
-        <div className="genreTilte border-gradient border-gradient-purple only-bottom">
-            <h1>Bacon ipsum dolor amet pork andouille dolor picanha proident ball tip spare ribs?</h1>
-        </div>
-        <div className="genreDescBox border-gradient border-gradient-purple only-bottom">
-            <p>Beef filet mignon pork belly commodo lorem ea aute eiusmod do.</p>
-        </div>
-    </div>
-    <div className="genreOne-content-container border-gradient border-gradient-purple">
-        <div className="genreTilte border-gradient border-gradient-purple only-bottom">
-            <h1>Bacon ipsum dolor amet pork andouille dolor picanha proident ball tip spare ribs?</h1>
-        </div>
-        <div className="genreDescBox border-gradient border-gradient-purple only-bottom">
-            <p>Beef filet mignon pork belly commodo lorem ea aute eiusmod do.</p>
-        </div>
-    </div>
-        <Footer page={"albums"}/>
+        <Footer page={"home"}/>
         </>
     )
 }
