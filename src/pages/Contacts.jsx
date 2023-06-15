f00d27f5d69912c7f0445456f9184df21f715e73
GTX155 <kirchoabv@gmail.com>
2023-06-15 22:01:32 +0300

    genres reorder, css file for contacts and about pages

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,14 +1,17 @@
 import { Header } from "/src/components/Header";
 import Footer from "/src/components/Footer";
+import "/src/pages/InfoPages.css"
 
 export function Contacts (){
     return (
         <>
             <Header />
-            <h1 style={{color: "#FBFEF9", textAlign: 'center'}}>Contacts</h1>
-            <a href="https://github.com/gtx155/TheNextStep">
-                <h2 style={{color: "#FBFEF9", textAlign: 'center'}}>Github</h2>
+            <h1 className="CA-Title">Contacts</h1>
+            <div className="ContactsContainer">
+            <a href="https://github.com/gtx155/TheNextStep" className="item border-gradient border-gradient-purple only-bottom">
+                Github
             </a>
+            </div>
             <Footer page={"absolute"}/>
         </>
     )
